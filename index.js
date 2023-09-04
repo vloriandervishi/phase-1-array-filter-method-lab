51265a1e05ffb0b6cfe465e87e01c7382cf25c4d
vlorian <vlorian.dervishi@gmail.com>
2023-09-04 14:48:52 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, driverNames) {
+  return drivers.filter(
+    (check) => check.toLowerCase() === driverNames.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, driverName) {
+  return drivers.filter((driverCheck) => {
+    if (driverCheck[0] === driverName[0]) {
+      return true;
+    }
+  });
+}
+
+function matchName(drivers, driverName) {
+  const fin = drivers.filter((driverCheck) =>
+    driverCheck.name === driverName ? true : false
+  );
+  return fin;
+}
